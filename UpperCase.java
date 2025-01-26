5dd314230223ebe8b3c7bb5a8fcfa7871132b7d0
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-26 18:43:40 +0530

    Add files via upload

---
+++ UpperCase.java
@@ -0,0 +1,9 @@
+public class UpperCase {
+    public static void main(String[]args){
+        String str="Hello world";
+        String upperstr=str.toUpperCase();
+        System.out.println("Uppercase:"+upperstr);
+
+    }
+    
+}
